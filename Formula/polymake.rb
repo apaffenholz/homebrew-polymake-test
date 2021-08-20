0d30e18052eab32f3ecb5635def74dfcf9fd0e33
Andreas Paffenholz <paffenholz@mathematik.tu-darmstadt.de>
2021-08-20 17:38:05 +0200

    polymake test formula/fix

--- Formula/polymake.rb
+++ Formula/polymake.rb
@@ -1,5 +1,5 @@
 class Polymake < Formula
-  desc "Tool for computations in algorithmic discrete geometry"
+  desc "A Tool for computations in algorithmic discrete geometry and algebra"
   homepage "https://polymake.org/"
   url "https://polymake.org/lib/exe/fetch.php/download/polymake-4.4.tar.bz2"
   sha256 "f4e9a3fe101bc8735c1531389bc2e7e364381ebdea22ee93c5a4e836ac6f6961"
